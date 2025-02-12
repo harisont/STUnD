4c4ebacb4912c8aad58ab97d567494cbd8d57178
Arianna Masciolini <uzkamascio@gmail.com>
2025-02-12 16:27:24 +0100

    Revert "syn"
    This reverts commit 0befc89360195e9aeb82aede0ea9e78239f61071.

--- static/stund.js
+++ static/stund.js
@@ -550,7 +550,6 @@ async function queryData(formData) {
   Highlights divergences in the results
 */
 function highlight(element, indices) {
-	console.log(indices)
     if (document.getElementById("textMode").checked) {
 	for (const i in indices) {
 	    var mark = document.createElement("span")
